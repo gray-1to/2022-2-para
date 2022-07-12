3847893b6bc1cb9f0578467659c7ed4b2794f368
gray-1to <graystin88@gmail.com>
2022-07-12 20:51:58 +0900

    2途中

--- Para4/src/para/Main07.java
+++ Para4/src/para/Main07.java
@@ -7,6 +7,8 @@ import para.graphic.target.*;
 import para.graphic.shape.*;
 import para.graphic.parser.MainParser;
 
+import java.util.concurrent.*;
+
 /** クライアントからの通信を受けて描画するサーバプログラム。
  * 監視ポートは30000番
  */
@@ -16,6 +18,9 @@ public class Main07{
   final Target target;
   final ShapeManager[] sms;
   final ServerSocket ss;
+  ExecutorService exec; // added
+  //Executor exec; // added
+  private boolean[] usedSms;
 
   /** 受け付け用ソケットを開くこと、受信データの格納場所を用意すること
    * を行う
@@ -24,6 +29,7 @@ public class Main07{
     target = new JavaFXTarget("Server", 320*MAXCONNECTION, 240);
     //target = new TextTarget(System.out);
     ServerSocket tmp=null;
+    exec = Executors.newFixedThreadPool(MAXCONNECTION);
     try{
       tmp = new ServerSocket(PORTNO);
     }catch(IOException ex){
@@ -34,6 +40,10 @@ public class Main07{
     sms = new ShapeManager[MAXCONNECTION];
     for(int i=0;i<MAXCONNECTION;i++){
       sms[i] = new OrderedShapeManager();
+    }
+    usedSms = new boolean[MAXCONNECTION];
+    for(int i=0;i<MAXCONNECTION;i++){
+      usedSms[i] = false;
     }
   }
 
@@ -65,28 +75,54 @@ public class Main07{
   public void start(){
     int i=0;
     while(true){
-      try(Socket s = ss.accept()){
-        BufferedReader r =
-          new BufferedReader(new InputStreamReader(s.getInputStream()));
-        ShapeManager dummy = new ShapeManager();
-        sms[i].clear();
-        sms[i].put(new Rectangle(10000*i,320*i,0,320,240,
-                                 new Attribute(0,0,0,true)));
-        MainParser parser
-          = new MainParser(new TranslateTarget(sms[i],
-                            new TranslationRule(10000*i, new Vec2(320*i,0))),
-                           dummy);
-        parser.parse(new Scanner(r));
+      try{
+        Socket s = ss.accept();
+        while(usedSms[i]){
+          System.err.println(i + " is used");
+          i=(i+1)%MAXCONNECTION;
+        }
+        usedSms[i] = true;
+        exec.execute(new MyThread(s, sms[i], i));
+        i=(i+1)%MAXCONNECTION;
       }catch(IOException ex){
         System.err.print(ex);
       }
-      i=(i+1)%MAXCONNECTION;
     }
   }
-  
+
+  class MyThread extends Thread{
+    final Socket s;
+    ShapeManager sm;
+    int i;
+
+    public MyThread(Socket s, ShapeManager sm, int i){
+      this.s = s;
+      this.sm = sm;
+      this.i = i;
+    }
+
+    public void run(){
+      try{
+        BufferedReader r = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        ShapeManager dummy = new ShapeManager();
+        sm.clear();
+        sm.put(new Rectangle(10000*i,320*i,0,320,240,
+                                   new Attribute(0,0,0,true)));
+        MainParser parser
+          = new MainParser(new TranslateTarget(sm,
+                            new TranslationRule(10000*i, new Vec2(320*i,0))),
+                            dummy);
+        parser.parse(new Scanner(r));
+        usedSms[i] = false;
+      }catch(IOException ex){
+        System.err.print(ex);
+      }
+    }
+  }
+
   public static void main(String[] args){
     Main07 m = new Main07();
     m.init();
     m.start();
-  } 
+  }
 }

9e64d946565ff91a7aeb5b410fc94e4723275bba
gray-1to <graystin88@gmail.com>
2022-07-22 11:41:59 +0900

    課題1

--- Para6/resource/para/opencl/max3.cl
+++ Para6/resource/para/opencl/max3.cl
@@ -1,2 +1,12 @@
 #pragma OPENCL EXTENSION cl_khr_byte_addressable_store : enable
 
+__kernel void Max(__constant float* a, __constant float* b, __constant float* c,
+                  __global float* d, int numElements){
+  // get index into global data array
+  int i = get_global_id(0);
+  
+  if(numElements<i) return;
+  
+  d[i] = max(a[i], max(b[i], c[i]));
+}
+
--- Para6/src/para/opencl/Sum3.java
+++ Para6/src/para/opencl/Sum3.java
@@ -26,11 +26,76 @@ public class Sum3{
     cl.initContext();
 
 
+    FloatBuffer tmpfb;
+    tmpfb = loadData("data/dataa.txt");
+    CLBuffer<FloatBuffer> BufferA = cl.createBuffer(tmpfb,READ_ONLY);
+    tmpfb.rewind();
+    tmpfb = loadData("data/datab.txt");
+    CLBuffer<FloatBuffer> BufferB = cl.createBuffer(tmpfb,READ_ONLY);
+    tmpfb.rewind();
+    tmpfb = loadData("data/datac.txt");
+    CLBuffer<FloatBuffer> BufferC = cl.createBuffer(tmpfb,READ_ONLY);
+    tmpfb.rewind();
+    CLBuffer<FloatBuffer> BufferD = cl.createFloatBuffer(tmpfb.limit(),
+							 WRITE_ONLY); 
+    int datasize = tmpfb.limit();
+    CLCommandQueue queue = cl.createQueue();
 
+    CLProgram program = cl.createProgramFromResource(this,"sum3.cl");
+    CLKernel kernel = program.createCLKernel("Sum");
+    kernel.putArgs(BufferA, BufferB, BufferC, BufferD);
+    kernel.setArg(4,datasize);
 
+    BufferD.getBuffer().rewind();
+    //デバイスへ転送、並列演算、演算結果の取得
+    queue.putWriteBuffer(BufferA, false)//BufferAのデータをカーネル側へ転送指令
+      .putWriteBuffer(BufferB, false)//BufferBのデータをカーネル側へ転送指令
+      .putWriteBuffer(BufferC, false)//BufferBのデータをカーネル側へ転送指令
+      .putBarrier() //今までの指令がすべて完了するまで待つ
+      .put1DRangeKernel(kernel, 0, datasize, 1)//演算指令
+      .putBarrier() //今までの指令がすべて完了するまで待つ
+      .putReadBuffer(BufferD, true);//BufferDのデータをホスト側へ転送指令
+                                    //転送完了まで待つ
+
+    //演算結果の出力
+    FloatBuffer fb = BufferD.getBuffer();
+    fb.rewind();
+    for(int i=0;i<fb.limit();i++){
+      System.out.print(fb.get()+" ");
+    }
+    System.out.println();
 
     cl.release();
   }
+
+  /** 引数で指定されたファイルから浮動小数の数値列を読み込み、その値を格納したバッファを返す。
+   * {@link CLBuffer}に組み込むBufferは
+   * @param fname 読み込み対象となるファイルの名
+   * @return ダイレクトバッファとして領域確保した後、読み込んだ値を書き込んだ{@link FloatBuffer}
+   */
+  static public FloatBuffer loadData(String fname){
+    ArrayList<Float> flt = new ArrayList<Float>();
+    Scanner sc=null;
+    try{
+      sc= new Scanner(new File(fname));
+    }catch(FileNotFoundException e){
+      System.err.println(e);
+      System.exit(1);
+    }
+    while(sc.hasNextFloat()){flt.add(sc.nextFloat());}
+    FloatBuffer fb = 
+      ByteBuffer.allocateDirect(flt.size()*java.lang.Float.SIZE/8) 
+                 // java.lang.Float.SIZE はfloat型のbit数を意味する定数 
+      .order(ByteOrder.nativeOrder())//float型を表すバイト順序にnativeを指定
+      .asFloatBuffer();
+    int i=0;
+    while(fb.hasRemaining()){
+      fb.put(flt.get(i));
+      i++;
+    }
+    fb.rewind();
+    return fb;
+  } 
 
   public static void main(String[] argv){
     new Sum3();

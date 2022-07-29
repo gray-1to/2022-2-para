6efa12e503852d733439fd72ffe0703d37105ba5
gray-1to <graystin88@gmail.com>
2022-07-29 12:03:45 +0900

    課題1

--- Para7/src/para/Main16.java
+++ Para7/src/para/Main16.java
@@ -1,3 +1,5 @@
+// 学籍番号：20B30100
+// 氏名：伊藤悠馬
 package para;
 
 import para.Main15;
--- Para7/src/para/Main22.java
+++ Para7/src/para/Main22.java
@@ -1,4 +1,13 @@
+// 学籍番号：20B30100
+// 氏名：伊藤悠馬
 package para;
+
+import para.Main15;
 
 /** スライダにより画像処理効果が変わるプログラム
  */
+public class Main22 extends Main15{
+    public Main22(){
+        super("imagefilter.cl", "Filter8");
+    }
+}
